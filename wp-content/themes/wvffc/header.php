6290ceec44ebd818d7175a54972f21e884c0a3d1
robbiesh <sherrard.rob@gmail.com>
2014-09-18 15:10:01 -0400

    header fix

--- wp-content/themes/wvffc/header.php
+++ wp-content/themes/wvffc/header.php
@@ -7,37 +7,35 @@
 </head>
 <body <?php body_class(); ?>>
 <header>
-  <div class="container header-nav">
+  <div class="container main-nav">
       <div id="logo">
         <img src="<?php echo get_template_directory_uri(); ?>/assets/img/WVFFC_Logo.png" />
       </div>
       <div id="mainNav">
-        <div class="header-nav-main">
-          <?php if(has_nav_menu('main_nav')){
-              $defaults = array(
-              	'theme_location'  => 'main_nav',
-              	'menu'            => 'main_nav',
-              	'container'       => false,
-              	'container_class' => '',
-              	'container_id'    => '',
-              	'menu_class'      => 'menu',
-              	'menu_id'         => '',
-              	'echo'            => true,
-              	'fallback_cb'     => 'wp_page_menu',
-              	'before'          => '',
-              	'after'           => '',
-              	'link_before'     => '',
-              	'link_after'      => '',
-              	'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-              	'depth'           => 0,
-              	'walker'          => ''
-              ); wp_nav_menu( $defaults );
-            }else{
-              echo "<p><em>main_nav</em> doesn't exist! Create it and it'll render here.</p>";
-            } ?>
-            <div id="search">
-              <i class="fa fa-search"></i>
-            </div>
+        <?php if(has_nav_menu('main_nav')){
+            $defaults = array(
+            	'theme_location'  => 'main_nav',
+            	'menu'            => 'main_nav',
+            	'container'       => false,
+            	'container_class' => '',
+            	'container_id'    => '',
+            	'menu_class'      => 'menu',
+            	'menu_id'         => '',
+            	'echo'            => true,
+            	'fallback_cb'     => 'wp_page_menu',
+            	'before'          => '',
+            	'after'           => '',
+            	'link_before'     => '',
+            	'link_after'      => '',
+            	'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+            	'depth'           => 0,
+            	'walker'          => ''
+            ); wp_nav_menu( $defaults );
+          }else{
+            echo "<p><em>main_nav</em> doesn't exist! Create it and it'll render here.</p>";
+          } ?>
+          <div id="search">
+            <i class="fa fa-search"></i>
           </div>
       </div>
       <div id="donate">
