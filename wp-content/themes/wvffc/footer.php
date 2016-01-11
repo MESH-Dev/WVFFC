5a57c9b31f3f1d7891629d3af7e8e9901577a41c
shaundover <shaun@meshfresh.com>
2016-01-11 10:31:26 -0500

    Update footer.php

--- wp-content/themes/wvffc/footer.php
+++ wp-content/themes/wvffc/footer.php
@@ -10,12 +10,12 @@
 <div class="container bottom">
   <div class="bottom-footer">
     <div class="bottom-footer-left">
-      <strong>West Virginia Food & Farm Coalition</strong><br/>
+      <strong>West Virginia Food &amp; Farm Coalition</strong><br/>
       102 East Maple St. Suite #5, Fayetville, WV 25840<br/>
       <strong>T</strong> (304) 877-7920 <strong>E</strong> info@foodandfarm.org
     </div>
     <div class="bottom-footer-right">
-      Design & Development<br/>
+      Design &amp; Development<br/>
       by <strong><a href="http://meshfresh.com" target="_blank">MESH</a></strong><br/>
       © 2014
     </div>
